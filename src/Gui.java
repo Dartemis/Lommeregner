add956772d2043f3fcf2a13db2a6cd10ee36cf62
Dartemis <patrick.rorth@gmail.com>
2015-03-13 11:32:27 +0100

    TEST 3

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,17 @@
+import javax.swing.*;
 
-public class Gui {
+public class Gui extends JFrame {
+	
+	public Gui( String title ){
+		super( title );
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setGuiSize();
+		
+	}
+	
+	public void setGuiSize(){
+		setSize(500, 500);
+	}
 
 }
